c0822521538a7e38eb460ec6c1b461021f182e87
SARAN R <saran.zoro2@gmail.com>
2026-06-15 17:21:33 +0530

    Time: 4 ms (82.18%), Space: 46.2 MB (47.65%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,12 +3,10 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-
+        char[] a=s.toCharArray();
+        char[] b=t.toCharArray();
         Arrays.sort(a);
         Arrays.sort(b);
-
         return Arrays.equals(a, b);
 
     }
