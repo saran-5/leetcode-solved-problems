74e56389ec85934108b68b17d0e9eb0ccc6b0e23
SARAN R <saran.zoro2@gmail.com>
2026-06-16 12:53:11 +0530

    Time: 14 ms (8.3%), Space: 52.5 MB (6.91%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,38 +1,22 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-         int n = board.length;
-        Set<Character>[] rows = new HashSet[n];
-        Set<Character>[] cols = new HashSet[n];
-        Set<Character>[] box = new HashSet[n];
-        for (int i = 0; i < n; i++) {
-            rows[i] = new HashSet<Character>();
-            cols[i] = new HashSet<Character>();
-            box[i] = new HashSet<Character>();
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                char curr = board[i][j];
-                if (curr == '.') {
-                    continue;
-                }
-                if (rows[i].contains(curr)) {
+        HashSet<String> set = new HashSet<>();
+
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+
+                char c = board[i][j];
+
+                if(c == '.') continue;
+
+                if(!set.add(c + "r" + i) ||
+                   !set.add(c + "c" + j) ||
+                   !set.add(c + "b" + (i/3) + (j/3))) {
                     return false;
-                } else {
-                    rows[i].add(curr);
-                }
-                if (cols[j].contains(curr)) {
-                    return false;
-                } else {
-                    cols[j].add(curr);
-                }
-                int boxIdx = (i / 3) * 3 + (j / 3);
-                if (box[boxIdx].contains(curr)) {
-                    return false;
-                } else {
-                    box[boxIdx].add(curr);
                 }
             }
         }
+
         return true;
     }
 }
