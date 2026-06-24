4d532f133fd20db93acb057a883fcd0b74f45829
SARAN R <saran.zoro2@gmail.com>
2026-06-24 14:10:11 +0530

    Time: 1 ms (99.7%), Space: 47.3 MB (38.08%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,14 +3,10 @@ class Solution {
         int ans = 0; 
         int end = 0; 
         int farthest = 0;
-        for(int i = 0; i < nums.length - 1; ++i){
+        for(int i = 0; i < nums.length - 1; i++){
             farthest = Math.max(farthest, i + nums[i]);
-            if(farthest >= nums.length - 1){
-                ++ans;
-                break;
-            }
             if( i == end ){
-                ++ans; 
+                ans++; 
                 end = farthest; 
             }             
         }
