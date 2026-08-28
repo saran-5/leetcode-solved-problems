6117dfd546700736f9069996745c1f42865b5846
SARAN R <saran.zoro2@gmail.com>
2026-08-28 12:38:12 +0530

    Time: 473 ms (99.84%), Space: 0B (100%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT name AS Customers
+FROM Customers
+LEFT JOIN Orders
+    ON customers.id = orders.customerId
+WHERE orders.customerId IS NULL;
