b8efcbc347344801d7ee76bcdae6dc4a12b3c848
SARAN R <saran.zoro2@gmail.com>
2026-08-31 09:45:27 +0530

    Time: 794 ms (58.24%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,5 +1,3 @@
-# Write your MySQL query statement below
-# Write your MySQL query statement below
 select p.product_name , sum(unit)as unit from Products p
 left join Orders o on p.product_id = o.product_id 
 where o.order_date like '2020-02-%'
